e0e2aa671617828be8eb993f5d6d2681641414b0
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-08-28 23:21:44 +0900

    Create ListPage.dart

---
+++ Speech/lib/screen/ListPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatefulWidget {
+  @override
+  State<ListPage> createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
