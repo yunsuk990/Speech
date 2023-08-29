059a2485a51bc6a0ae3fc0844050e417f9ae2bcf
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-08-29 23:52:54 +0900

    Update main.dart

--- Speech/lib/main.dart
+++ Speech/lib/main.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:speech/FirstPage.dart';
 import 'package:speech/screen/MemoPage.dart';
 import 'package:speech/screen/folderPage.dart';
 import 'MyHomePage.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  Future<Database> initDatabase() async {
+    return openDatabase(
+      join(await getDatabasesPath(), 'speech_database_db'),
+      onCreate: (db, version){
+        return db.execute(
+            'CREATE TABLE folder(id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                'name TEXT, dateTime TEXT)'
+        );
+      },
+      version: 1
+    );
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+    Future<Database> database = initDatabase();
     return MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(),
       routes: {
-        '/folder':(context) => FolderPage(),
+        '/':(context) => MyHomePage(database),
+        '/folder':(context) => FolderPage(database),
         '/memo': (context) => MemoPage()
       }
     );
