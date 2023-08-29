6bce0b2db02b158c0d67be899038a71a62bd530b
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-08-29 23:52:55 +0900

    Update Folder.dart

--- Speech/lib/modal/Folder.dart
+++ Speech/lib/modal/Folder.dart
@@ -2,10 +2,19 @@ import 'package:flutter/cupertino.dart';
 
 class Folder {
   String name;
-  FocusNode focusNode;
-  TextEditingController controller;
+  FocusNode? focusNode;
+  TextEditingController? controller;
   String dateTime = "";
 
   Folder(this.name, this.focusNode, this.controller, this.dateTime);
 
+  Folder.fromJson(Map data): name = data['contentid'], dateTime = data['dateTime'];
+
+
+  Map<String, dynamic> toMap(){
+    return{
+      'name' : name,
+      'dateTime': dateTime
+    };
+  }
 }
