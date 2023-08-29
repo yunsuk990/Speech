c5bd88bcab749b5602ba1a78f6d5efadb45948df
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-08-29 23:52:52 +0900

    Update FirstPage.dart

--- Speech/lib/FirstPage.dart
+++ Speech/lib/FirstPage.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:sqflite_common/sqlite_api.dart';
 
 import 'modal/Folder.dart';
 
 class FirstPage extends StatefulWidget{
+  final Future<Database> database;
+  FirstPage(this.database);
+
   @override
   State<StatefulWidget> createState()  => _firstPage();
 }
 
 class _firstPage extends State<FirstPage>{
 
-  List<Folder> list = new List.empty(growable: true);
-  int index = -1;
+  late Future<List<Folder>?> list;
+  bool newFolder = false;
 
   @override
   void initState() {
     super.initState();
-    list.add(Folder("a",FocusNode(), TextEditingController(text: "a"),DateTime.now().toIso8601String()));
-    list.add(Folder("b",FocusNode(), TextEditingController(text: "b"),DateTime.now().toIso8601String()));
-    list.add(Folder("c",FocusNode(), TextEditingController(text: "c"),DateTime.now().toIso8601String()));
-    list.add(Folder("d",FocusNode(), TextEditingController(text: "d"),DateTime.now().toIso8601String()));
-    list.add(Folder("e",FocusNode(), TextEditingController(text: "e"),DateTime.now().toIso8601String()));
+    list = getFolderList();
   }
 
   @override
@@ -41,24 +41,36 @@ class _firstPage extends State<FirstPage>{
                     CupertinoIcons.folder_badge_plus, color: Colors.white,
                     size: 30),
                 padding: EdgeInsets.only(right: 10),
-                onPressed: () {
-                  // showCupertinoDialog(context: context, builder: (context){
-                  //   return CupertinoAlertDialog(
-                  //     title: Text('폴더 제목'),
-                  //     content: CupertinoTextField(),
-                  //     actions: [
-                  //       CupertinoButton(child: Text('확인'), onPressed: (){
-                  //         Navigator.of(context).pop();
-                  //       })
-                  //     ],
-                  //   );
-                  // });
-                  setState(() {
-                    list.add(Folder("무제폴더",FocusNode(), TextEditingController(text: "무제폴더"), DateTime.now().toIso8601String()));
+                onPressed: () async {
+                  List<String>? items = await list.then((value){
+                    return List.generate(value!.length, (index){
+                      return value[index].name;
                     });
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    FocusScope.of(context).requestFocus(list[list.length-1].focusNode);
                   });
+                  int index = 0;
+                  for(int i=0;i<items!.length; i++){
+                    if(items.contains("무제폴더${i+1}")){
+                      index = i+2;
+                    }
+                  }
+                  if(index == 0){
+                    if(items.contains('무제폴더')){
+                      FocusNode focusNode = FocusNode();
+                      insertTour(Folder("무제폴더1",focusNode, TextEditingController(text: "무제폴더${index}"), DateTime.now().toIso8601String()));
+                      FocusScope.of(context).requestFocus(focusNode);
+                    }else{
+                      FocusNode focusNode = FocusNode();
+                      insertTour(Folder("무제폴더",focusNode, TextEditingController(text: "무제폴더${index}"), DateTime.now().toIso8601String()));
+                      FocusScope.of(context).requestFocus(focusNode);
+                    }
+                  }else{
+                    FocusNode focusNode = FocusNode();
+                    insertTour(Folder("무제폴더${index}",focusNode, TextEditingController(text: "무제폴더${index}"), DateTime.now().toIso8601String()));
+                    WidgetsBinding.instance.addPostFrameCallback((_){
+                      FocusScope.of(context).requestFocus(focusNode);
+                    });
+                  }
+                  newFolder = true;
                 })
           ],
         ),
@@ -77,58 +89,75 @@ class _firstPage extends State<FirstPage>{
                           child: Container(
                               width: width,
                               height: height - 240,
-                              child: GridView.builder(
-                                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount: 3),
-                                  itemCount: list.length,
-                                  padding: EdgeInsets.only(bottom: 80),
-                                  itemBuilder: (BuildContext context, int index) {
-                                    return Container(
-                                      child:  Column(
-                                        children: <Widget>[
-                                          CupertinoContextMenu(
-                                              actions: <Widget>[
-                                                CupertinoContextMenuAction(
-                                                  child: Text('삭제'),
-                                                  isDestructiveAction: true,
-                                                  onPressed: () { setState(() {
-                                                    list.removeAt(index);
-                                                    Navigator.of(context).pop();
-                                                  });},
-                                                  trailingIcon: CupertinoIcons.delete,),
-                                                CupertinoContextMenuAction(
-                                                  child: Text('이름 변경'),
-                                                  isDefaultAction: true,
-                                                  onPressed: () {
-                                                    this.index = index;
-                                                    list[index].focusNode.requestFocus();
-                                                    Navigator.pop(context);},
-                                                  trailingIcon: CupertinoIcons.pencil,),
-                                              ],
-                                              child: CupertinoButton(onPressed: () {
-                                                  Navigator.of(context,).pushNamed('/folder', arguments: list[index].name);
-                                                }, child: Icon(CupertinoIcons.folder_fill, size: 90),padding: EdgeInsets.zero,
-                                              ),
+                              child: FutureBuilder(
+                                builder: (context, snapshot){
+                                  switch(snapshot.connectionState){
+                                    case ConnectionState.none:
+                                      return CupertinoActivityIndicator();
+                                    case ConnectionState.waiting:
+                                      return CupertinoActivityIndicator();
+                                    case ConnectionState.active:
+                                      return CupertinoActivityIndicator();
+                                    case ConnectionState.done:
+                                      if(snapshot.hasData){
+                                        return GridView.builder(
+                                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                                crossAxisCount: 3),
+                                            itemCount: (snapshot.data! as List<Folder>).length,
+                                            padding: EdgeInsets.only(bottom: 80),
+                                            itemBuilder: (BuildContext context, int index) {
+                                              List<Folder> folderList = snapshot.data as List<Folder>;
 
-                                          ),
-                                          TextField(
-                                            style: TextStyle(color: Colors.black),
-                                            autofocus: false,
-                                            decoration: null,
-                                            keyboardType: TextInputType.text,
-                                            focusNode: list[index].focusNode,
-                                            textAlign: TextAlign.center,
-                                            controller: list[index].controller,
-                                            onChanged: (text){
-                                              list[index].name = text;
-                                              list[index].controller = TextEditingController(text: text);
-                                              print(list[index].name);
-                                            },
-                                          )
-                                        ],
-                                      ),
-                                    );
+                                              return Container(
+                                                child:  Column(
+                                                  children: <Widget>[
+                                                    CupertinoContextMenu(
+                                                      actions: <Widget>[
+                                                        CupertinoContextMenuAction(
+                                                          child: Text('삭제'),
+                                                          isDestructiveAction: true,
+                                                          onPressed: () { setState(() {
+                                                            deleteFolder(folderList[index]);
+                                                            Navigator.of(context).pop();
+                                                          });},
+                                                          trailingIcon: CupertinoIcons.delete,),
+                                                        CupertinoContextMenuAction(
+                                                          child: Text('이름 변경'),
+                                                          isDefaultAction: true,
+                                                          onPressed: () {
+                                                            folderList![index].focusNode!.requestFocus();
+                                                            Navigator.pop(context);},
+                                                          trailingIcon: CupertinoIcons.pencil,),
+                                                      ],
+                                                      child: CupertinoButton(onPressed: () {
+                                                        Navigator.of(context,).pushNamed('/folder', arguments: folderList![index].name);
+                                                      }, child: Icon(CupertinoIcons.folder_fill, size: 90),padding: EdgeInsets.zero,
+                                                      ),
+                                                    ),
+                                                    TextField(
+                                                        style: TextStyle(color: Colors.black),
+                                                        autofocus: false,
+                                                        decoration: null,
+                                                        keyboardType: TextInputType.text,
+                                                        focusNode: folderList![index].focusNode,
+                                                        textAlign: TextAlign.center,
+                                                        controller: folderList![index].controller,
+                                                        onSubmitted: (text){
+                                                          print(folderList![index].name);
+                                                          updateFolder(folderList![index], text);
+                                                        }
+                                                    ),
+                                                  ],
+                                                ),
+                                              );
+                                            }
+                                        );
+                                      }else{
+                                        return Text('No Data');
+                                      }
                                   }
+                                },
+                                future: list,
                               )
                           )
                       )
@@ -137,10 +166,55 @@ class _firstPage extends State<FirstPage>{
             )
                 ,onTap: (){
                     FocusScope.of(context).unfocus();
+                    newFolder = false;
               },
         )
 
 
     );
+  }
+
+  void insertTour(Folder folder) async {
+    final Database database = await widget.database!;
+    await database
+        .insert('folder', folder.toMap(), conflictAlgorithm: ConflictAlgorithm.fail).then((value){
+          setState(() {
+            list = getFolderList();
+          });
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('폴더를 생성하였습니다.')));
+    });
+  }
+
+  Future<List<Folder>> getFolderList() async {
+    final Database database = await widget.database!;
+    final List<Map<String, dynamic>> maps = await database.query('folder');
+    return List.generate(maps.length, (index){
+      return Folder(maps[index]['name'], FocusNode(), TextEditingController(text: maps[index]['name']), DateTime.now().toIso8601String());
+    });
+  }
+
+  void deleteFolder(Folder folder) async {
+    final Database database = await widget.database!;
+    await database.delete('folder',
+        where: 'name=?', whereArgs: [folder.name]).then((value){
+          setState(() {
+            list = getFolderList();
+          });
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('폴더를 삭제하였습니다.')));
+    });
+  }
+
+  void updateFolder(Folder folder, String name) async {
+    final Database database = await widget.database;
+    await database.update('folder',
+    {
+      'name' : name,
+      'dateTime' : folder.dateTime
+    },
+    where: 'name = ?', whereArgs: [folder.name]).then((value){
+      setState(() {
+        list = getFolderList();
+      });
+    });
   }
 }
