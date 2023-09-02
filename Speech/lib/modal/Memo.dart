0b754ecc0e64c1e3f5556eec6e122a708bcd573c
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-09-02 18:19:56 +0900

    Update Memo.dart

--- Speech/lib/modal/Memo.dart
+++ Speech/lib/modal/Memo.dart
@@ -1,14 +1,17 @@
 
 class Memo {
   int? id;
+  String? title;
   String folderName;
   String? content = null;
-  String dateTime = "";
+  String dateTime;
 
-  Memo(this.id,this.folderName, this.content, this.dateTime);
+  Memo(this.id,this.title,this.folderName, this.content, this.dateTime);
 
   Map<String, dynamic> toMap(){
     return{
+      'id' : id,
+      'title' : title,
       'folderName' : folderName,
       'content' : content,
       'dateTime': dateTime
