bc1d176361f5831ada7587214831f6970f82730b
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-09-07 23:53:11 +0900

    Update Memo.dart

--- Speech/lib/modal/Memo.dart
+++ Speech/lib/modal/Memo.dart
@@ -1,19 +1,28 @@
 
+import 'package:flutter/cupertino.dart';
+import 'package:speech/modal/Speech.dart';
+
 class Memo {
   int? id;
   String? title;
   String folderName;
-  String? content = null;
+  List<Speech>? content;
   String dateTime;
 
   Memo(this.id,this.title,this.folderName, this.content, this.dateTime);
 
   Map<String, dynamic> toMap(){
+    Map<String, dynamic> sp = {};
+
+    for(int i=0; i<content!.length; i++){
+      sp.addAll(content![i].toMap());
+    }
+
     return{
       'id' : id,
       'title' : title,
       'folderName' : folderName,
-      'content' : content,
+      'content' : sp,
       'dateTime': dateTime
     };
   }
