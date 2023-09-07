ed399d3182a31a4e453c0f91bda9a5d3ab8a57d8
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-09-07 23:53:14 +0900

    Update main.dart

--- Speech/lib/main.dart
+++ Speech/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
-import 'package:speech/FirstPage.dart';
+import 'package:speech/screen/FirstPage.dart';
 import 'package:speech/screen/MemoPage.dart';
 import 'package:speech/screen/UpdateMemo.dart';
 import 'package:speech/screen/folderPage.dart';
