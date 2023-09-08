bb2a2cf1ac88c704067f2fc511246821df0c1d10
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-09-08 23:42:09 +0900

    Update MyHomePage.dart

--- Speech/lib/MyHomePage.dart
+++ Speech/lib/MyHomePage.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:speech/screen/ListPage.dart';
 import 'package:sqflite_common/sqlite_api.dart';
 import 'screen/FirstPage.dart';
 
 class MyHomePage extends StatefulWidget{
-  final Future<Database> database;
-  MyHomePage(this.database);
+  FirebaseDatabase? _database;
+  DatabaseReference? reference;
+  MyHomePage(this._database, this.reference);
 
   @override
   State<StatefulWidget> createState() => _myHomePage();
@@ -26,7 +28,7 @@ class _myHomePage extends State<MyHomePage> with SingleTickerProviderStateMixin 
   Widget build(BuildContext context) {
     return Scaffold(
       body: TabBarView(
-          children: <Widget>[FirstPage(widget.database), ListPage()],
+          children: <Widget>[FirstPage(widget._database, widget.reference), ListPage()],
           controller: controller,
       ),
       bottomNavigationBar:
