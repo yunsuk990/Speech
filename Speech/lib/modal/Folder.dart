0ca545ecc75d8ae5ecb48f85e95e3c92706fb354
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-09-10 20:44:21 +0900

    Update Folder.dart

--- Speech/lib/modal/Folder.dart
+++ Speech/lib/modal/Folder.dart
@@ -2,16 +2,18 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 
 class Folder {
+  String? id;
   String name;
   FocusNode? focusNode;
   TextEditingController? controller;
   String dateTime = "";
 
-  Folder(this.name, this.focusNode, this.controller, this.dateTime);
+  Folder(this.id, this.name, this.focusNode, this.controller, this.dateTime);
 
-  Folder.fromJson(Map data): name = data['contentid'], dateTime = data['dateTime'];
+  Folder.fromJson(Map data): id = data['id'], name = data['contentid'], dateTime = data['dateTime'];
 
   Folder.fromSnapshot(DataSnapshot snapshot):
+        id = snapshot.key,
         name = (snapshot.value as Map)['name'],
         focusNode = FocusNode(),
         controller = TextEditingController(text: (snapshot.value as Map)['name']),
@@ -19,6 +21,7 @@ class Folder {
 
   toJson() {
     return {
+      'id' : id,
       'name': name,
       'dateTime': dateTime,
     };
@@ -26,6 +29,7 @@ class Folder {
 
   Map<String, dynamic> toMap(){
     return{
+      'id' : id,
       'name' : name,
       'dateTime': dateTime
     };
