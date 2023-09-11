57ee3c85133b65cf28f6f64b48e2134c4d8afed2
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-09-11 21:30:13 +0900

    Update folderPage.dart

--- Speech/lib/screen/folderPage.dart
+++ Speech/lib/screen/folderPage.dart
@@ -5,8 +5,6 @@ import '../modal/Folder.dart';
 import '../modal/Memo.dart';
 
 class FolderPage extends StatefulWidget {
-  // final Future<Database> database;
-
   FirebaseDatabase? _database;
   DatabaseReference? reference;
   FolderPage(this._database, this.reference);
@@ -16,27 +14,17 @@ class FolderPage extends StatefulWidget {
 }
 
 class _FolderPageState extends State<FolderPage> {
-
-  late Future<List<Memo>>? memoList;
   late Folder folder;
-
   @override
   void initState() {
     super.initState();
   }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-  }
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     double width = size.width;
     double height = size.height;
     folder = ModalRoute.of(context)!.settings.arguments as Folder;
-    // memoList = getMemoList(folderName, widget.reference);
 
     return Scaffold(
       appBar: AppBar(
@@ -57,12 +45,7 @@ class _FolderPageState extends State<FolderPage> {
               child: Image.asset('images/edit.png', color: Colors.white),
               onPressed: () {
                 Navigator.of(context)
-                    .pushNamed('/memo', arguments: folder.name)
-                    .then((_) {
-                  setState(() {
-                    // memoList = getMemoList(folderName);
-                  });
-                });
+                    .pushNamed('/memo', arguments: folder.name);
               })
         ],
       ),
@@ -73,25 +56,23 @@ class _FolderPageState extends State<FolderPage> {
             Container(
                 width: width,
                 height: height * 0.83,
-                child: FutureBuilder(
-                    future: widget.reference?.child("memo").orderByChild("folderName").equalTo(folder.name).once(),
+                child: StreamBuilder(
+                    stream: widget.reference
+                        ?.child("memo")
+                        .orderByChild("folderName")
+                        .equalTo(folder.name)
+                        .onValue,
                     builder: (context, snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.none:
-                      return CupertinoActivityIndicator();
-                    case ConnectionState.waiting:
-                      return CupertinoActivityIndicator();
-                    case ConnectionState.active:
-                      return CupertinoActivityIndicator();
-                    case ConnectionState.done:
                       if (snapshot.hasData) {
+                        print(snapshot.data?.snapshot.value);
                         return ListView.builder(
                             itemCount: snapshot.data?.snapshot.children.length,
                             padding: EdgeInsets.only(top: 10),
                             itemBuilder: (BuildContext context, int index) {
-                              List<Memo> folderList = List.empty(growable: true);
-                              for(final child in snapshot.data!.snapshot.children){
-                                folderList.add(Memo.fromSnapshot(child));
+                              List<Memo> memoList = List.empty(growable: true);
+                              for (final child
+                                  in snapshot.data!.snapshot.children) {
+                                memoList.add(Memo.fromSnapshot(child));
                               }
                               return Card(
                                   elevation: 5,
@@ -102,66 +83,45 @@ class _FolderPageState extends State<FolderPage> {
                                   child: Padding(
                                     padding: EdgeInsets.all(10),
                                     child: ListTile(
-                                      leading: Text(
-                                          '${folderList[index].title}',
+                                      leading: Text('${memoList[index].title}',
                                           style: TextStyle(
                                               fontWeight: FontWeight.bold,
                                               fontSize: 18)),
                                       title: Text(
-                                        "",
+                                        "${memoList[index].speechTitle?[0]}",
                                         style: TextStyle(
                                             fontWeight: FontWeight.w300),
                                         maxLines: 1,
                                       ),
                                       trailing:
-                                          Text('${folderList[index].dateTime}'),
+                                          Text('${memoList[index].dateTime}'),
                                       onTap: () {
                                         Navigator.of(context)
                                             .pushNamed('/memoUpdate',
-                                                arguments: folderList[index])
-                                            .then((value) {
-                                          setState(() {
-                                            // memoList = getMemoList(folderName);
-                                          });
-                                        });
+                                                arguments: memoList[index]);
                                       },
                                       onLongPress: () {
-                                        // deleteMemo(folderList[index], folderList[index].folderName);
+                                        deleteMemo(
+                                            memoList[index], widget.reference);
                                       },
                                     ),
                                   ));
                             });
                       } else {
-                        return Text('No Data');
+                        return Center(child: Text('No Data'));
                       }
-                  }
-                }))
+                    }))
           ],
         ),
       )),
     );
   }
 
-  // Future<List<Memo>> getMemoList(String folderName, DatabaseReference? reference) async {
-  //   reference?.child("memo").orderByChild(folderName).onValue.listen((event) {
-  //       // List<Memo> list = List.empty(growable: true);
-  //       // for (final child in event.snapshot.children) {
-  //       //   list.add(Memo.fromSnapshot(child));
-  //       // }
-  //           return List.generate(event.snapshot.children.length, (index){
-  //             return Memo.fromSnapshot(event.snapshot.children.elementAt(index));
-  //       });
-  //   });
-  // }
-  //
-  // void deleteMemo(Memo memo, String folderName) async {
-  //   final Database database = await widget.database!;
-  //   await database.delete('memo',
-  //       where: 'id=?', whereArgs: [memo.id]).then((value){
-  //     setState(() {
-  //       memoList = getMemoList(folderName);
-  //     });
-  //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('메모를 삭제하였습니다.')));
-  //   });
-  // }
+  void deleteMemo(Memo memo, DatabaseReference? reference) async {
+    await reference?.child("memo").child(memo.id!).remove().then((_) {
+      print(memo.id!);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('메모를 삭제하였습니다.')));
+    });
+  }
 }
