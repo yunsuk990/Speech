8c3bfe5fe22e4899c87e94bc1af7fe93fe3832df
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-09-17 18:22:06 +0900

    Update Memo.dart

--- Speech/lib/modal/Memo.dart
+++ Speech/lib/modal/Memo.dart
@@ -1,5 +1,6 @@
 
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,17 +10,22 @@ class Memo {
   String? id;
   String? title;
   String folderName;
-  Map<String, dynamic> speech = Map();
+  List<dynamic> speech = List.empty(growable: true);
   String dateTime;
 
   Memo(this.id,this.title,this.folderName, this.speech, this.dateTime);
 
   Map<String, dynamic> toMap(){
+    List<Object> list = List.empty(growable: true);
+    for(Speech item in speech){
+      list.add((item.toMap()));
+    }
+
     return{
       'id' : id,
       'title' : title,
       'folderName' : folderName,
-      'speech' : jsonEncode(speech),
+      'speech' : list,
       'dateTime': dateTime
     };
   }
@@ -28,8 +34,13 @@ class Memo {
         id = snapshot.key,
         title = (snapshot.value as Map)['title'],
         folderName = (snapshot.value as Map)['folderName'],
-        speech = jsonDecode((snapshot.value as Map)['speech']),
-        dateTime = (snapshot.value as Map)['dateTime']{
+        dateTime = (snapshot.value as Map)['dateTime']
+  {
+    List<dynamic> list = List.empty(growable: true);
+    for(final item in (snapshot.value as Map)['speech']){
+      list.add(Speech(item["title"], item["content"], TextEditingController(text: item["title"]), TextEditingController(text: item["content"])));
+    }
+    speech = list;
   }
 
 }
